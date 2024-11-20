2b2da892478995085901fc2382bc83990af2b87e
Slimo300 <sebastian.slimak707@gmail.com>
2024-11-21 00:39:55 +0100

    requireAuth from common

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,30 +1,6 @@
-import { NotAuthorizedError } from "@spellcinema/lib";
-import express, {NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import express, { Request, Response } from "express";
 
-// import { requireAuth } from "@spellcinema/lib";
-
-interface UserPayload {
-    id: string,
-    email: string,
-    isAdmin: boolean,
-}
-
-const requireAuth = (publicKey: string) => (req: Request, res: Response, next: NextFunction) => {
-    if (!req.cookies.accessToken) {
-        throw new NotAuthorizedError();
-    }
-
-    try {
-        const { id, email, isAdmin } = jwt.verify(req.cookies.accessToken, publicKey) as UserPayload;
-        req.currentUser = { id, email, isAdmin };
-    } catch (err) {
-        console.error(err);
-        throw new NotAuthorizedError();
-    }
-
-    next();
-}
+import { requireAuth } from "@spellcinema/lib";
 
 const currentUserRouter = (publicKey: string): express.Router => {
     const router = express.Router();
