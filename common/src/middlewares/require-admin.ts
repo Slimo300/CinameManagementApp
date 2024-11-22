956e905415af75489fabac593b07ae94fe9fd966
Slimo300 <sebastian.slimak707@gmail.com>
2024-11-23 00:25:13 +0100

    Updates

--- common/src/middlewares/require-admin.ts
+++ common/src/middlewares/require-admin.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { ForbiddenError } from '../errors/forbidden-error';
 
-export const requireAdmin = () => (req: Request, res: Response, next: NextFunction) => {
+export const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
   if (!req.currentUser?.isAdmin) {
     throw new ForbiddenError("user not allowed to perform this action");
   }
